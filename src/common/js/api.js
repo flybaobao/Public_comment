a7d56003f717cd8f4b0ff2e7946a7df1a6898ebb
flybaobao <m1713538469@163.com>
2018-08-16 23:24:48 +0800

    api

--- src/common/js/api.js
+++ src/common/js/api.js
@@ -1,8 +1,9 @@
 // 本地调试时
-// let serverUrl = '/api'
+let serverUrl = '/api'
 // 打包部署上线时
 // let serverUrl = 'http://104.223.19.16'
-let serverUrl = 'http://www.evelyntan.cn'
+// let serverUrl = 'http://www.evelyntan.cn'
+// let serverUrl = 'http://www.shixiaoli.com'
 export default {
-  dataUrl: serverUrl + '/assets/video/data.json'
+  dataUrl: serverUrl + '/data.json'
 }
